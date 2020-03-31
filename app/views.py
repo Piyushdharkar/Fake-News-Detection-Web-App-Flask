9653c82cba75bfcb94b1b6876c450e2f634b94df
Piyush <Piyush.Dharkar@gmail.com>
2020-04-01 01:18:33 +0530

    Added templates, bootstrap, css, js files. Added basic upload news article page.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,13 @@
 from app import app
+from flask import render_template, request
 
 @app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home():
-    return 'Test home page'
+    return render_template('/public/index.html')
+
+@app.route('/output', methods=['GET', 'POST'])
+def get_output():
+    print(request.form.get('news_article_text'))
+
+    return render_template('/public/output.html')
