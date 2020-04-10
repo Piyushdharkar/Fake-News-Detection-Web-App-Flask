b40d0ef4f1b3af7fadfe2b2df0be27547ab5e132
Piyush <Piyush.Dharkar@gmail.com>
2020-04-10 15:05:01 +0530

    Removed bar plot controller.

--- app/Controllers.py
+++ app/Controllers.py
@@ -8,19 +8,3 @@ class Fake_news_classifier_controller():
 
     def predict(self, text):
         return pd.DataFrame({'class':['Real', 'Fake'], 'probability': [0.3, 0.7]})
-
-class Bar_plot_controller():
-    def __init__(self):
-        pass
-
-
-    def fit(self, dataset, x, y, fig_size=(15, 15)):
-        self.dataset = dataset
-        self.x = x
-        self.y = y
-        self.fig_size = fig_size
-
-    def draw(self):
-        fig = plt.figure(figsize=self.fig_size)
-        sns.barplot(x=self.x, y=self.y, data=self.dataset)
-        return fig
