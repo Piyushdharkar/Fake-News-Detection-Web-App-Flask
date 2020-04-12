3efbaf15dee2f801b07448b6637b9a72ec5b4bcb
Piyush <Piyush.Dharkar@gmail.com>
2020-04-12 16:57:00 +0530

    Made some functions private.

--- app/controllers.py
+++ app/controllers.py
@@ -7,7 +7,6 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 import numpy as np
 import torch
-import pickle
 
 from app.models import Pickle_model, Fake_news_classifier_model
 
@@ -16,13 +15,13 @@ class Preprocessor_controller():
         self.vectorizer_path = vectorizer_path
         self.device = device
 
-        self.init_stemmer()
-        self.init_vectorizer()
+        self.__init_stemmer()
+        self.__init_vectorizer()
 
     def get_vectorizer_vocab_size(self):
         return len(self.vectorizer.vocabulary)
 
-    def init_stemmer(self):
+    def __init_stemmer(self):
         nltk.download('stopwords')
 
         stop = set(stopwords.words('english'))
@@ -32,22 +31,22 @@ class Preprocessor_controller():
         self.stopwords = stop
         self.stemmer = PorterStemmer()
 
-    def init_vectorizer(self):
+    def __init_vectorizer(self):
         pickle_model = Pickle_model()
 
         vocabulary = pickle_model.get_pickle(path=self.vectorizer_path)
         self.vectorizer = CountVectorizer(stop_words='english', vocabulary=vocabulary)
         self.vectorizer._validate_vocabulary()
 
-    def stem_text(self, text):
+    def __stem_text(self, text):
         final_text = [self.stemmer.stem(word.strip()) for word in text.split() if word.strip().lower() not in self.stopwords]
 
         return " ".join(final_text)
 
-    def vectorize_text(self, text):
+    def __vectorize_text(self, text):
         return self.vectorizer.transform(text)
 
-    def csr_to_tensor(self, csr):
+    def __csr_to_tensor(self, csr):
         coo = csr.tocoo()
         values = coo.data
         indices = np.vstack((coo.row, coo.col))
@@ -64,11 +63,11 @@ class Preprocessor_controller():
 
         inputs = [title + " " + text for (title, text) in zip(titles, text)]
 
-        stemmed_text = [self.stem_text(text) for text in inputs]
+        stemmed_text = [self.__stem_text(text) for text in inputs]
         print("Stemmed_text: {}".format(stemmed_text))
-        vector = self.vectorize_text(stemmed_text)
+        vector = self.__vectorize_text(stemmed_text)
         print("Vector: {}".format(vector))
-        tensor = self.csr_to_tensor(vector)
+        tensor = self.__csr_to_tensor(vector)
         print("Tensor: {}".format(tensor))
         return tensor.to(self.device)
 
@@ -79,11 +78,11 @@ class Fake_news_classifier_controller():
         self.model.load_model(path=model_path, device=device)
         self.model.eval()
 
-    def predict_tensor(self, tensor):
+    def __predict_tensor(self, tensor):
         pred = self.model(tensor)
         return pred
 
     def predict(self, tensor):
-        prob_list = self.predict_tensor(tensor).tolist()
+        prob_list = self.__predict_tensor(tensor).tolist()
 
         return [{'real':{'label':'Real', 'value':real_prob}, 'fake':{'label':'Fake', 'value':fake_prob}} for fake_prob, real_prob in prob_list]
